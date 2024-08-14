03e78d6e363377ac1618ac08a2bcc3ef2f14c074
yokamoto5742-h <yokamoto5742@gmail.com>
2024-08-14 19:34:30 +0900

    1.0.0の開発開始

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ from barcode.codex import Code128
 from barcode.writer import ImageWriter
 from io import BytesIO
 
-VERSION = "0.1.3"
-LAST_UPDATED = "2024/07/22"
+VERSION = "1.0.0"
+LAST_UPDATED = "2024/08/14"
 
 # config.iniファイルの読み込み
 config = configparser.ConfigParser()
